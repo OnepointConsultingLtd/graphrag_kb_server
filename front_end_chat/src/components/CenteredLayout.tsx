ea7f15e297580fdeb4732d683ae96643293f5abc
Murtaza Jafari <65913750+jafarimurtaza@users.noreply.github.com>
2025-06-18 18:49:04 +0300

    Fixed layout issue

--- front_end_chat/src/components/CenteredLayout.tsx
+++ front_end_chat/src/components/CenteredLayout.tsx
@@ -1,12 +1,16 @@
-
-export default function CenteredLayout({ title, children }: { title: string, children: React.ReactNode }) {
-
-    return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <div className="w-full mb-2">
-                <h2 className="border-b-1 border-gray-300 pb-2 mb-4">{title}</h2>
-            </div>
-            {children}
-        </div>
-    )
+export default function CenteredLayout({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen mx-4">
+      <div className="w-full mb-2">
+        <h2 className="border-b-1 border-gray-300 pb-2 mb-4">{title}</h2>
+      </div>
+      {children}
+    </div>
+  );
 }
