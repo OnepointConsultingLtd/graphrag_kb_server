c8ac04a455c6db45eb88e3db3b0bfa865a505ae8
Gil Fernandes <gil.fernandes@onepointltd.com>
2025-07-23 15:17:07 +0100

    Added related topics support based on keywords extracted from text

--- graphrag_kb_server/service/graphrag/related_topics.py
+++ graphrag_kb_server/service/graphrag/related_topics.py
@@ -6,6 +6,14 @@ from graphrag_kb_server.model.topics import SimilarityTopics, SimilarityTopicsRe
 from graphrag_kb_server.service.graphrag.graph_support import read_graphrag_project
 from graphrag_kb_server.service.similar_topics import get_sorted_related_entities_simple_rerank
 from graphrag_kb_server.utils.cache import GenericSimpleCache
+from graphrag_kb_server.service.graphrag.query import prepare_local_search
+from graphrag_kb_server.model.rag_parameters import ContextParameters
+
+
+from graphrag.query.context_builder.entity_extraction import (
+    
+    map_query_to_entities,
+)
 
 
 graph_cache = GenericSimpleCache[nx.classes.graph.Graph, Path]()
@@ -17,6 +25,29 @@ def get_related_topics_graphrag(request: SimilarityTopicsRequest) -> SimilarityT
     if G is None:
         G = read_graphrag_project(request.project_dir)
         graph_cache.set(project_dir, G)
+    
+    if not request.source:
+        search_engine = prepare_local_search(ContextParameters(query=request.text, project_dir=request.project_dir, context_size=5000))
+        context_builder = search_engine.context_builder
+        entities = map_query_to_entities(
+            query=request.text,
+            text_embedding_vectorstore=context_builder.entity_text_embeddings,
+            text_embedder=context_builder.text_embedder,
+            all_entities_dict=context_builder.entities,
+            embedding_vectorstore_key=context_builder.embedding_vectorstore_key,
+            include_entity_names=[],
+            exclude_entity_names=[],
+            k=8,
+            oversample_scaler=2,
+        )
+        titles = [entity.title for entity in entities if entity.title in G.nodes()]
+        if len(titles) > 0:
+            params = request.model_dump()
+            params["source"] = titles[0]
+            request = SimilarityTopicsRequest(**params)
+        else:
+            return None
+    
     if not request.source in G.nodes():
         return None
     return get_sorted_related_entities_simple_rerank(G, request)
