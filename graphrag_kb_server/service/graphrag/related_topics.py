4c2a5ca2795e664e06d81046ecdb372a68118629
Gil Fernandes <gil.fernandes@onepointltd.com>
2025-07-23 13:11:18 +0100

    Refactor server configuration and enhance related topics caching
    - Updated the server configuration to use the current location host for BASE_SERVER and WEBSOCKET_SERVER.
    - Implemented caching for the graph in the related topics endpoint to improve performance by reducing redundant reads from the project directory.

--- graphrag_kb_server/service/graphrag/related_topics.py
+++ graphrag_kb_server/service/graphrag/related_topics.py
@@ -1,11 +1,22 @@
+from pathlib import Path
+import networkx as nx
+
 from graphrag_kb_server.model.engines import Engine
 from graphrag_kb_server.model.topics import SimilarityTopics, SimilarityTopicsRequest
 from graphrag_kb_server.service.graphrag.graph_support import read_graphrag_project
 from graphrag_kb_server.service.similar_topics import get_sorted_related_entities_simple_rerank
+from graphrag_kb_server.utils.cache import GenericSimpleCache
+
+
+graph_cache = GenericSimpleCache[nx.classes.graph.Graph, Path]()
 
 
 def get_related_topics_graphrag(request: SimilarityTopicsRequest) -> SimilarityTopics | None:
-    G = read_graphrag_project(request.project_dir)
+    project_dir = request.project_dir
+    G = graph_cache.get(project_dir)
+    if G is None:
+        G = read_graphrag_project(request.project_dir)
+        graph_cache.set(project_dir, G)
     if not request.source in G.nodes():
         return None
     return get_sorted_related_entities_simple_rerank(G, request)
