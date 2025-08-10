f09a40adf1302efff783ae34d54b6c59508d2f7b
Gil Fernandes <gil.fernandes@onepointltd.com>
2025-08-10 20:03:12 +0100

    Added some more instructions run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+source .venv/bin/activate
+uv sync
+
+# Build GraphRAG front end
+cd front_end
+cp server_prod.js server.ts
+yarn
+yarn build
+cd ..
+
+# Build front end chat
+# nvm use v22.12.0
+# cd front_end_chat
+# cp src/lib/server_prod.ts src/lib/server.ts
+# yarn
+# yarn build
+# cd ..
+
 cd "$(dirname "$0")"
 uv sync
 uv run ./graphrag_kb_server/main/webapp.py
