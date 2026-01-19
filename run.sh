8d47cb5e67509bc3a10f2654265e29eb6d01d1ff
Gil Fernandes <gil.fernandes@onepointltd.com>
2026-01-19 13:23:58 +0000

    Updated run.sh

--- run.sh
+++ run.sh
@@ -12,18 +12,18 @@ source .venv/bin/activate
 # yarn build
 # cd ..
 
-# Build front end chat
-# nvm use v22.12.0
-# cd front_end_chat
-# cp src/lib/server_prod.ts src/lib/server.ts
-# yarn
-# yarn build
-# VERSION=1
-# sed -i "s/index-fixed\.css?v=[0-9]\+/index-fixed.css/g" ./front_end_chat/dist/index.html
-# sed -i "s/index-fixed.css/index-fixed.css?v=$VERSION/g" ./front_end_chat/dist/index.html
-# sed -i "s/index-fixed\.js?v=[0-9]\+/index-fixed.js/g" ./front_end_chat/dist/index.html
-# sed -i "s/index-fixed.js/index-fixed.js?v=$VERSION/g" ./front_end_chat/dist/index.html
-# cd ..
+Build front end chat
+nvm use v22.12.0
+cd front_end_chat
+cp src/lib/server_prod.ts src/lib/server.ts
+yarn
+yarn build
+VERSION=1.0.1
+sed -i "s/index-fixed\.css?v=[0-9]\+/index-fixed.css/g" ./front_end_chat/dist/index.html
+sed -i "s/index-fixed.css/index-fixed.css?v=$VERSION/g" ./front_end_chat/dist/index.html
+sed -i "s/index-fixed\.js?v=[0-9]\+/index-fixed.js/g" ./front_end_chat/dist/index.html
+sed -i "s/index-fixed.js/index-fixed.js?v=$VERSION/g" ./front_end_chat/dist/index.html
+cd ..
 
 cd "$(dirname "$0")"
 /home/ubuntu/.local/bin/uv sync
