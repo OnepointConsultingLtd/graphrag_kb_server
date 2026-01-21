732fb5c4a82c8122aae514cc33c0d2d28f5e9cac
Gil Fernandes <gil.fernandes@onepointltd.com>
2026-01-21 12:35:38 +0000

    Added fix dfor missing text_units_context

--- graphrag_kb_server/service/lightrag/lightrag_init.py
+++ graphrag_kb_server/service/lightrag/lightrag_init.py
@@ -1,9 +1,11 @@
 from pathlib import Path
+import os
+import json
 
 from lightrag import LightRAG
 from graphrag_kb_server.service.lightrag.lightrag_constants import LIGHTRAG_FOLDER
 from lightrag.llm.openai import openai_embed
-from lightrag.kg.shared_storage import initialize_pipeline_status
+from lightrag.kg.shared_storage import get_namespace_data, initialize_pipeline_status
 
 from graphrag_kb_server.service.lightrag.lightrag_model_support import select_model_func
 from graphrag_kb_server.utils.cache import GenericProjectSimpleCache
@@ -26,8 +28,47 @@ async def initialize_rag(project_folder: Path) -> LightRAG:
         llm_model_func=llm_model_func,
     )
     await rag.initialize_storages()
+    await initialize_storages(rag)
     await initialize_pipeline_status()
     lightrag_cache.set(project_folder, rag)
     # Enable LLM cache for now
-    rag.llm_response_cache.global_config["enable_llm_cache"] = True
+    rag.llm_response_cache.global_config["enable_llm_cache"] = False
     return rag
+
+
+async def initialize_storages(rag: LightRAG):
+    """
+    Makes sure that the storages are initialized and the data is loaded from the file.
+    This fixes problems with non loaded JSON storage data.
+    """
+    for storage in (
+        rag.full_docs,
+        rag.text_chunks,
+        rag.full_entities,
+        rag.full_relations,
+        rag.entity_chunks,
+        rag.relation_chunks,
+        rag.entities_vdb,
+        rag.relationships_vdb,
+        rag.chunks_vdb,
+        rag.chunk_entity_relation_graph,
+        rag.llm_response_cache,
+        rag.doc_status,
+    ):
+        if not storage:
+            continue
+
+        file_name = getattr(storage, "_file_name", None)
+        if file_name:
+            loaded_data = load_json(file_name) or {}
+            storage._data = await get_namespace_data(storage.final_namespace)
+            storage._data.update(loaded_data)
+        else:
+            await storage.initialize()
+
+
+def load_json(file_name):
+    if not os.path.exists(file_name):
+        return None
+    with open(file_name, encoding="utf-8-sig") as f:
+        return json.load(f)
