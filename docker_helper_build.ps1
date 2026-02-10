1e4c2537d5c7f5c2060b85382a2a2d4eedbbd441
Gil Fernandes <gil.fernandes@onepointltd.com>
2026-02-10 18:18:51 +0000

    Added shell script to build the Docker file

---
+++ docker_helper_build.ps1
@@ -0,0 +1,16 @@
+docker system prune -f
+
+$IMAGE_NAME = "graphrag_kb_server_helper"
+$TAG = "v0.1.0"
+
+# Change name according to your name
+docker build -t ${IMAGE_NAME}:${TAG} -f Dockerfile.helper .
+
+$AWS_REGION = "eu-west-2"
+$REPO_NAME="knowledge_server/${IMAGE_NAME}"
+$ECR_REGISTRY="303872128380.dkr.ecr.eu-west-2.amazonaws.com"
+$ECR_IMAGE="${ECR_REGISTRY}/${REPO_NAME}:${TAG}"
+
+aws ecr get-login-password --region $AWS_REGION --profile onepoint | docker login --username AWS --password-stdin $ECR_REGISTRY
+docker tag ${IMAGE_NAME}:${TAG} $ECR_IMAGE
+docker push $ECR_IMAGE
