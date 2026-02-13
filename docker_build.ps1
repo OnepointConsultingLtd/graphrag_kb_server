20b5008641d144313c6254c2891ad1d0906f2a2c
Gil Fernandes <gil.fernandes@onepointltd.com>
2026-02-13 15:14:52 +0000

    Updated docker version

--- docker_build.ps1
+++ docker_build.ps1
@@ -1,7 +1,7 @@
 docker system prune -f
 
 $IMAGE_NAME = "graphrag_kb_server"
-$TAG = "v0.9.1"
+$TAG = "v0.9.4"
 
 # Use this script to build the docker image
 # You need to have uv and docker installed
